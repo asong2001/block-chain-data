6cd73a1fd265f4ab18226ddfbe502dd9b0a4857e
阿松 <11612001@mail.sustc.edu.cn>
2019-01-18 17:27:22 +0800

    增加功能
    增加保存为excel文件的功能。

--- bc_value.py
+++ bc_value.py
@@ -4,8 +4,26 @@ import tushare as ts
 
 pro = ts.pro_api('338fb37f45430d7f9699295040b4a0374d84f5b1850d53b29980fa2e')
 
-# pro = ts.pro_api()
+# exchange name
+exchange_name = 'okex'
 
-df = pro.query('coinbar', exchange='okex', symbol='btcusdt', freq='daily', start_date='20180801', end_date='20180830')
+# exchange pair
+coin_pair = 'btcusdt'
+
+# pair of coin
+fr = 'daily'
+
+# start date
+s_date = '20180801'
+
+# end date
+e_date = '20180830'
+
+df = pro.query('coinbar', exchange=exchange_name, symbol=coin_pair, freq=fr, start_date=s_date, end_date=e_date)
 
 print(df)
+
+file_name = exchange_name + '_' + coin_pair + '_' + fr + '_' + s_date + '_' + e_date
+
+# 保存
+df.to_excel('excel/' + file_name + '.xlsx')
