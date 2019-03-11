f8610343683de39c3d26b7bf5d518fc188251cf5
阿松 <11612001@mail.sustech.edu.cn>
2019-03-11 14:49:56 +0800

    修改时间

--- binance_top4_v1.py
+++ binance_top4_v1.py
@@ -15,16 +15,21 @@ def main():
     time_server = client.get_server_time()
     ms = time_server['serverTime']
     ms = datetime.datetime.fromtimestamp(ms / 1000.0)
-    print(time_server)
+    print(ms)
 
     symbol = ["BTCUSDT", "LTCUSDT", "ETHUSDT", "EOSUSDT", "BNBUSDT"]
     data = []
 
     # request time
     origin_time = time.localtime()
-    req_time = time.strftime("%d %b,%Y", origin_time)
-    stop_time = req_time + time.strftime(" %H %M %S", origin_time)
-    print('Stop time' + stop_time)
+    req_time = time.strftime("%d %b,%Y ", origin_time)
+    # 截止到当前时间的上一个整点时刻
+    stop_time = req_time + time.strftime("%H %M", origin_time)
+
+    # 北京时间
+    bj_time = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M")
+    print('Beijing time:' + bj_time)
+    print('Stop time:' + stop_time)
     for s in symbol:
         # fetch weekly klines since it listed
         klines = client.get_historical_klines(s, Client.KLINE_INTERVAL_1DAY, req_time)
@@ -33,8 +38,8 @@ def main():
     content = data
 
     # build excel
-    # fileName = stop_time + '.xls'
     fileName = 'latest.xls'
+    # fileName = stop_time + '.xls'
     workbook = Workbook(encoding='utf-8')
     xlsheet = workbook.add_sheet("1", cell_overwrite_ok=True)
     table_head = [ 
@@ -64,10 +69,11 @@ def main():
             if col == 0:
                 xlsheet.write(row + 1, col, symbol[row])
             elif col == 6:
-                ms = content[row][col]
-                ldate = datetime.datetime.fromtimestamp(ms / 1000.0)
-                ldate = str(ldate)
+                # ms = content[row][col]
+                # ldate = datetime.datetime.fromtimestamp(ms / 1000.0)
+                ldate = str(bj_time)
                 xlsheet.write(row + 1, col, ldate)
+
             else:
                 xlsheet.write(row + 1, col, content[row][col])
 
